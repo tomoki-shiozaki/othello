c56640bc69e48b2a9805c2b115e5547585b5e860
shiozaki <shiozaki.tomoki2@gmail.com>
2025-05-30 09:32:54 +0900

    chore(settings): set ALLOWED_HOSTS as an environment variable

--- othello_project/settings.py
+++ othello_project/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["othello-d46f.onrender.com", "localhost", "127.0.0.1"]
-# ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["othello-d46f.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 
 # Application definition
